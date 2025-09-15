ac20a00f2b7ba7fb88f565924c08c94fda969c6f
Gaetano Pessa <nomeecognome@gmail.com>
2025-09-15 15:58:55 +0200

    fix

--- src/app/(home)/components/AboutUs/index.tsx
+++ src/app/(home)/components/AboutUs/index.tsx
@@ -6,7 +6,7 @@ import { Grid, Typography } from "@mui/material";
 import { PortableText } from "next-sanity";
 import { QUERYResult } from "../../../../../sanity.types";
 
-const AboutUs = ({ content }: QUERYResult["aboutUs"]) => (
+const AboutUs = ({ content }: Exclude<QUERYResult["aboutUs"], null>) => (
   <Section color="primary" id={SECTIONS.aboutUs}>
     <Grid container spacing={STANDARD_SPACING}>
       <Grid size={{ xs: 12, md: 5 }}>
