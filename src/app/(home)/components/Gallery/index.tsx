a2ff85005430ad463f0ab246834bbaf6305ad039
Gaetano Pessa <nomeecognome@gmail.com>
2025-09-15 16:10:33 +0200

    added video

--- src/app/(home)/components/Gallery/index.tsx
+++ src/app/(home)/components/Gallery/index.tsx
@@ -1,42 +1,53 @@
 "use client";
 import { Section } from "@/components";
-import { Box, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid";
 
 import { SECTIONS } from "@/contants";
 import { urlFor } from "@/sanity/lib/image";
 import { QUERYResult } from "../../../../../sanity.types";
+import { YouTubeEmbed } from "@next/third-parties/google";
 
 const Gallery: React.FC<QUERYResult["gallery"]> = (gallery) => {
   return (
     <Section color="secondary" id={SECTIONS.gallery}>
-      <Grid container spacing={4}>
-        {gallery?.images?.map(({ title, image }, index) => (
-          <Grid size={{ xs: 12, md: 4 }} key={index}>
-            <Box
-              component="figure"
-              sx={{
-                m: 0,
-              }}
-            >
+      <Stack gap={4}>
+        <Grid container spacing={4}>
+          {gallery?.images?.map(({ title, image }, index) => (
+            <Grid size={{ xs: 12, md: 4 }} key={index}>
               <Box
-                component="img"
-                src={urlFor(image.asset!._ref).width(400).height(400).url()}
-                alt={title}
-                loading="lazy"
+                component="figure"
                 sx={{
-                  width: "100%",
-                  height: "auto",
-                  display: "block",
+                  m: 0,
                 }}
-              />
-              <Typography component="figcaption" variant="caption">
-                {title}
-              </Typography>
-            </Box>
-          </Grid>
-        ))}
-      </Grid>
+              >
+                <Box
+                  component="img"
+                  src={urlFor(image.asset!._ref).width(400).height(400).url()}
+                  alt={title}
+                  loading="lazy"
+                  sx={{
+                    width: "100%",
+                    height: "auto",
+                    display: "block",
+                  }}
+                />
+                <Typography component="figcaption" variant="caption">
+                  {title}
+                </Typography>
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
+
+        <YouTubeEmbed
+          videoid="O0GGfZ6jOjE"
+          style={
+            "width: 100%; height: auto; max-width: unset;"
+            //   "width: 80%; max-width: 80%; justify-content: center; margin-left: 10%"
+          }
+        />
+      </Stack>
     </Section>
   );
 };
