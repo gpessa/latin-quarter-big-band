fce4c0144c764afe591f19176beae6780b87ef62
Gaetano Pessa <nomeecognome@gmail.com>
2025-09-15 15:53:38 +0200

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript/no-require-imports": "off"
+      "@typescript-eslint/no-require-imports": "off"
     }
   }
 ];
