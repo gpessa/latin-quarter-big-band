2c1aceea18d92050ca72ec8820a57cf39005153f
Gaetano Pessa <nomeecognome@gmail.com>
2025-09-22 12:32:44 +0200

    test

--- src/contants/index.tsx
+++ src/contants/index.tsx
@@ -5,8 +5,8 @@ export const GOOGLE_ANALYTICS = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS!;
 export const STANDARD_SPACING = 6;
 export const STANDARD_MARGIN_BOTTOM = 3;
 export const EMAILS = [
-  "nomeecognome@gmail.com",
   "info@latinquarterbigband.com",
+  // "nomeecognome@gmail.com",
 ];
 
 export const SECTIONS = {
