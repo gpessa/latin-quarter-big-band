5b79760f90d31026cda9cccf58908abcb32467b6
Gaetano Pessa <nomeecognome@gmail.com>
2025-09-25 10:16:26 +0200

    fkx

--- src/app/api/book-us/route.ts
+++ src/app/api/book-us/route.ts
@@ -4,12 +4,14 @@ import { client } from "@/sanity/lib/client";
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 console.log(
   "NEXT_PUBLIC_RESEND_API_KEY",
   process.env.NEXT_PUBLIC_RESEND_API_KEY
 );
+
+console.log("RESEND_API_KEY", process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
   const { name, email, phone, message }: BookUsFormData = await req.json();
